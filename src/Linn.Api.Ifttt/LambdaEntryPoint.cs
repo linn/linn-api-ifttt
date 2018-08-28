85115aeaccbc5129ea6c68fb93c440f7f8abdb44
Barry Williams <barry.williams@linn.co.uk>
2018-08-28 14:10:16 +0100

    Turn logging off

--- src/Linn.Api.Ifttt/LambdaEntryPoint.cs
+++ src/Linn.Api.Ifttt/LambdaEntryPoint.cs
@@ -1,13 +1,14 @@
 namespace Linn.Api.Ifttt
 {
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Logging;
 
     public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
     {
         protected override void Init(IWebHostBuilder builder)
         {
             builder
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().ConfigureLogging((ctx, logging) => { logging.AddFilter(null, LogLevel.Error); });
         }
     }
 }
