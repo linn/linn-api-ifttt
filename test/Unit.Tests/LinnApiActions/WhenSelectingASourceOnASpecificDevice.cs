c702b303cf323da54403ea946c29aee5e8f73e44
David Drysdale-Wilson <david.drysdale-wilson@linn.co.uk>
2018-12-21 00:04:29 +0000

    [#162731322] Added unit tests for selecting a device's source

---
+++ test/Unit.Tests/LinnApiActions/WhenSelectingASourceOnASpecificDevice.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+
+using FluentAssertions;
+
+using NSubstitute;
+
+using Xunit;
+
+namespace Linn.Api.Ifttt.Testing.Unit.LinnApiActions
+{
+    public class WhenSelectingASourceOnASpecificDevice : ContextBase
+    {
+        private readonly string result;
+
+        private readonly string accessToken;
+
+        private readonly string deviceId;
+
+        private readonly string sourceId;
+
+        public WhenSelectingASourceOnASpecificDevice()
+        {
+            this.accessToken = Guid.NewGuid().ToString();
+
+            this.deviceId = Guid.NewGuid().ToString();
+
+            this.sourceId = Guid.NewGuid().ToString();
+
+            this.RestClient.Put(
+                Arg.Any<CancellationToken>(),
+                Arg.Any<Uri>(),
+                Arg.Any<Dictionary<string, string>>(),
+                Arg.Any<Dictionary<string, string[]>>(),
+                Arg.Any<object>()).Returns(HttpStatusCode.OK);
+
+            this.result = this.Sut.SelectSource(this.accessToken, this.deviceId, this.sourceId, CancellationToken.None).Result;
+        }
+
+        [Fact]
+        public void ShouldReturnATimestamp()
+        {
+            DateTime.Parse(this.result).ToUniversalTime().Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(5));
+        }
+
+        [Fact]
+        public void ShouldSelectSourceOnTheDevice()
+        {
+            this.RestClient.Received().Put(
+                Arg.Any<CancellationToken>(),
+                Arg.Is<Uri>(uri => uri.ToString() == $"http://localhost/players/{this.deviceId}/source"),
+                Arg.Is<Dictionary<string, string>>(p => p["sourceId"] == this.sourceId),
+                Arg.Is<Dictionary<string, string[]>>(d => d["Authorization"][0] == $"Bearer {this.accessToken}"),
+                Arg.Any<object>());
+        }
+    }
+}
---
+++ test/Unit.Tests/LinnApiActions/WhenSelectingASourceOnASpecificDeviceFails.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+
+using FluentAssertions;
+
+using Linn.Api.Ifttt.Proxies;
+
+using NSubstitute;
+
+using Xunit;
+
+namespace Linn.Api.Ifttt.Testing.Unit.LinnApiActions
+{
+    public class WhenSelectingASourceOnASpecificDeviceFails : ContextBase
+    {
+        private readonly HttpStatusCode statusCode;
+
+        private readonly Action action;
+
+        public WhenSelectingASourceOnASpecificDeviceFails()
+        {
+            this.statusCode = HttpStatusCode.Unauthorized;
+
+            this.RestClient.Put(
+                Arg.Any<CancellationToken>(),
+                Arg.Any<Uri>(),
+                Arg.Any<Dictionary<string, string>>(),
+                Arg.Any<Dictionary<string, string[]>>(),
+                Arg.Any<object>()).Returns(this.statusCode);
+
+            this.action = () => this.Sut.SelectSource(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), CancellationToken.None).Wait();
+        }
+
+        [Fact]
+        public void ShouldThrowLinnApiException()
+        {
+            this.action.Should().Throw<LinnApiException>().And.StatusCode.Should().Be(this.statusCode);
+        }
+    }
+}
